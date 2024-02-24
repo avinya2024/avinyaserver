f8dd34fe30351ec1bf13e5c9d4f182028b60f33a
Gauravved <gaurav.rpandit1@gmail.com>
2024-02-24 13:10:50 +0530

    Solved path request error

--- controller/registrationController.js
+++ controller/registrationController.js
@@ -4,7 +4,7 @@ const Razorpay = require('razorpay');
 
 const razorpay = new Razorpay({
     key_id: 'rzp_test_7p7xCTeRWyvtgf',
-    key_secret: 'cPzOQNWdRDehhDx9F6Vis2EQ'
+    key_secret:'cPzOQNWdRDehhDx9F6Vis2EQ'
 });
 
 module.exports.order = async (req, res) => {
@@ -17,9 +17,9 @@ module.exports.order = async (req, res) => {
             currency: "INR",
             receipt: "rcptid_11",
         });
-
+    
         console.log("Order created:", order);
-
+    
         // Now you can access the properties of the order object
         res.json({
             orderId: order.id,
@@ -33,58 +33,58 @@ module.exports.order = async (req, res) => {
 }
 module.exports.verification = async (req, res) => {
     try {
-        let body = req.body.response.razorpay_order_id + "|" + req.body.response.razorpay_payment_id;
+        let body = req.body.response.razorpay_order_id + "|"+req.body.response.razorpay_payment_id;
         var crypto = require('crypto');
-        var expectedSignature = crypto.createHmac('sha256', 'cPzOQNWdRDehhDx9F6Vis2EQ')
-            .update(body.toString()).digest('hex');
-        console.log("signature received: " + req.body.response.razorpay_signature);
-        console.log("expected siignature: " + expectedSignature)
+        var expectedSignature = crypto.createHmac('sha256','cPzOQNWdRDehhDx9F6Vis2EQ')
+        .update(body.toString()).digest('hex');
+        console.log("signature received: "+req.body.response.razorpay_signature);
+        console.log("expected siignature: "+expectedSignature)
         var response = {
             "signatureIsValid": "false"
         }
-        if (expectedSignature == req.body.response.razorpay_signature) {
-            response = {
+        if(expectedSignature == req.body.response.razorpay_signature){
+            response= {
                 "signatureIsValid": "true"
             }
-            res.status(200).json({ razorpay_payment_id: req.body.response.razorpay_payment_id, razorpay_signature: expectedSignature })
+            res.status(200).json({razorpay_payment_id: req.body.response.razorpay_payment_id, razorpay_signature: expectedSignature})
         }
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Failed to verify payment' });
     }
 }
-module.exports.registration = async (req, res, next) => {
-    try {
-        if (req.method === 'POST') {
-            const { teamName, teamMembers, teamLeaderName, teamLeaderPhone, teamLeaderEmail, member2Name, member2Email, member3Name, member3Email, department, college, transactionID, paidAmount } = req.body;
-            const isValidEmail = await registrationSchema.findOne({ email: teamLeaderEmail });
-            const isValidEmail2 = await registrationSchema.findOne({ member2Email: member2Email });
-            const isValidEmail3 = await registrationSchema.findOne({ member3Email: member3Email });
-            if (isValidEmail) {
+module.exports.registration = async(req,res,next)=>{
+    try{
+        if(req.method === 'POST'){
+            const {teamName, teamMembers, teamLeaderName, teamLeaderPhone, teamLeaderEmail, member2Name, member2Email, member3Name, member3Email, department, college, transactionID, paidAmount} = req.body;
+            const isValidEmail = await registrationSchema.findOne({email: teamLeaderEmail});
+            const isValidEmail2 = await registrationSchema.findOne({member2Email: member2Email});
+            const isValidEmail3 = await registrationSchema.findOne({member3Email: member3Email});
+            if(isValidEmail){
                 let transactions = isValidEmail.transactionId
                 transactions.push(transactionID)
-                let amt = isValidEmail.paidAmount + paidAmount
-                await registrationSchema.findByIdAndUpdate(isValidEmail._id, { hackathon: true, paidAmount: amt, transactionId: transactions })
-                return res.status(200).json({ message: "Success" })
+                let amt = isValidEmail.paidAmount + paidAmount 
+                await registrationSchema.findByIdAndUpdate(isValidEmail._id,{hackathon: true, paidAmount: amt, transactionId: transactions})
+                return res.status(200).json({message:"Success"})
             }
-            if (isValidEmail2) {
+            if(isValidEmail2){
                 let transactions = isValidEmail2.transactionId
                 transactions.push(transactionID)
-                let amt = isValidEmail.paidAmount + paidAmount
-                await registrationSchema.findByIdAndUpdate(isValidEmail._id, { hackathon: true, paidAmount: amt, transactionId: transactions })
-                return res.status(200).json({ message: "Success" })
+                let amt = isValidEmail.paidAmount + paidAmount 
+                await registrationSchema.findByIdAndUpdate(isValidEmail._id,{hackathon: true, paidAmount: amt, transactionId: transactions})
+                return res.status(200).json({message:"Success"})
             }
-            if (isValidEmail3) {
+            if(isValidEmail3){
                 let transactions = isValidEmail3.transactionId
                 transactions.push(transactionID)
-                let amt = isValidEmail.paidAmount + paidAmount
-                await registrationSchema.findByIdAndUpdate(isValidEmail._id, { hackathon: true, paidAmount: amt, transactionId: transactions })
-                return res.status(200).json({ message: "Success" })
+                let amt = isValidEmail.paidAmount + paidAmount 
+                await registrationSchema.findByIdAndUpdate(isValidEmail._id,{hackathon: true, paidAmount: amt, transactionId: transactions})
+                return res.status(200).json({message:"Success"})
             }
-            const isValidPhone = await registrationSchema.findOne({ teamLeaderPhone: teamLeaderPhone });
-            if (isValidPhone) {
-                return res.status(400).json({ message: "Phone already registered" });
-            } else {
+            const isValidPhone = await registrationSchema.findOne({teamLeaderPhone: teamLeaderPhone});
+            if(isValidPhone){
+                return res.status(400).json({message: "Phone already registered"});
+            }else{
                 let transactions = [];
                 transactions.push(transactionID)
                 console.log(transactions);
@@ -104,47 +104,47 @@ module.exports.registration = async (req, res, next) => {
                     exhibition: true,
                     transactionId: transactions,
                     paidAmount: paidAmount
-                }).then(() => {
-                    res.status(200).json({ message: "Created" })
-                }).catch((err) => {
+                }).then(()=>{
+                    res.status(200).json({message: "Created"})
+                }).catch((err)=>{
                     console.log(err);
-                    res.status(400).json({ message: "Unable to create" })
+                    res.status(400).json({message: "Unable to create"})
                 })
             }
-
+            
 
         }
-        else if (req.method === 'PUT') {
+        else if(req.method === 'PUT'){
             console.log("Here");
-            const { teamLeaderName, teamLeaderPhone, teamLeaderEmail, department, college, techexpert, nontechexpert, transactionID, paidAmount } = req.body;
-            const isValidEmail = await registrationSchema.findOne({ teamLeaderEmail: teamLeaderEmail });
-            const isValidEmail2 = await registrationSchema.findOne({ member2Email: teamLeaderEmail });
-            const isValidEmail3 = await registrationSchema.findOne({ member3Email: teamLeaderEmail });
-            if (isValidEmail) {
+            const {teamLeaderName, teamLeaderPhone, teamLeaderEmail, department, college, techexpert, nontechexpert,transactionID, paidAmount} = req.body;
+            const isValidEmail = await registrationSchema.findOne({teamLeaderEmail: teamLeaderEmail});
+            const isValidEmail2 = await registrationSchema.findOne({member2Email: teamLeaderEmail});
+            const isValidEmail3 = await registrationSchema.findOne({member3Email: teamLeaderEmail});
+            if(isValidEmail){
                 let transactions = isValidEmail.transactionId
                 transactions.push(transactionID)
-                let amt = isValidEmail.paidAmount + paidAmount
-                await registrationSchema.findByIdAndUpdate(isValidEmail._id, { techexpert: techexpert, nontechexpert: nontechexpert, paidAmount: amt, transactionId: transactions })
-                return res.status(200).json({ message: "Success" })
+                let amt = isValidEmail.paidAmount + paidAmount 
+                await registrationSchema.findByIdAndUpdate(isValidEmail._id,{techexpert:techexpert,nontechexpert:nontechexpert, paidAmount: amt, transactionId: transactions})
+                return res.status(200).json({message:"Success"})
             }
-            if (isValidEmail2) {
+            if(isValidEmail2){
                 let transactions = isValidEmail.transactionId
                 transactions.push(transactionID)
-                let amt = isValidEmail.paidAmount + paidAmount
-                await registrationSchema.findByIdAndUpdate(isValidEmail._id, { techexpert: techexpert, nontechexpert: nontechexpert, paidAmount: amt, transactionId: transactions })
-                return res.status(200).json({ message: "Success" })
+                let amt = isValidEmail.paidAmount + paidAmount 
+                await registrationSchema.findByIdAndUpdate(isValidEmail._id,{techexpert:techexpert,nontechexpert:nontechexpert, paidAmount: amt, transactionId: transactions})
+                return res.status(200).json({message:"Success"})
             }
-            if (isValidEmail3) {
+            if(isValidEmail3){
                 let transactions = isValidEmail.transactionId
                 transactions.push(transactionID)
-                let amt = isValidEmail.paidAmount + paidAmount
-                await registrationSchema.findByIdAndUpdate(isValidEmail._id, { techexpert: techexpert, nontechexpert: nontechexpert, paidAmount: amt, transactionId: transactions })
-                return res.status(200).json({ message: "Success" })
+                let amt = isValidEmail.paidAmount + paidAmount 
+                await registrationSchema.findByIdAndUpdate(isValidEmail._id,{techexpert:techexpert,nontechexpert:nontechexpert, paidAmount: amt, transactionId: transactions})
+                return res.status(200).json({message:"Success"})
             }
-            const isValidPhone = await registrationSchema.findOne({ teamLeaderPhone: teamLeaderPhone });
-            if (isValidPhone) {
-                return res.status(400).json({ message: "Phone already registered" });
-            } else {
+            const isValidPhone = await registrationSchema.findOne({teamLeaderPhone: teamLeaderPhone});
+            if(isValidPhone){
+                return res.status(400).json({message: "Phone already registered"});
+            }else{
                 let transactions = []
                 transactions.push(transactionID)
                 await registrationSchema.create({
@@ -158,82 +158,86 @@ module.exports.registration = async (req, res, next) => {
                     nontechexpert: nontechexpert,
                     transactionId: transactions,
                     paidAmount: paidAmount
-                }).then(() => {
-                    res.status(200).json({ message: "Created" });
-                }).catch((err) => {
-                    res.status(400).json({ message: "Unable to create " + err })
+                }).then(()=>{
+                    res.status(200).json({message: "Created"});
+                }).catch((err)=>{
+                    res.status(400).json({message: "Unable to create "+err})
                 })
             }
         }
-        else if (req.method === 'PATCH') {
-            const { email, contact } = req.body;
-            const isValidEmail = await registrationSchema.findOne({ teamLeaderEmail: email, teamLeaderPhone: contact, hackathon: true });
-            const isValidEmail2 = await registrationSchema.findOne({ member2Email: email, teamLeaderPhone: contact, hackathon: true });
-            const isValidEmail3 = await registrationSchema.findOne({ member3Email: email, teamLeaderPhone: contact, hackathon: true });
-            if (isValidEmail) {
-                return res.status(200).json({ status: true });
+        else if(req.method === 'PATCH'){
+            const {email, contact} = req.body;
+                const isValidEmail = await registrationSchema.findOne({teamLeaderEmail: email, hackathon: true});
+                const isValidEmail2 = await registrationSchema.findOne({member2Email: email, hackathon: true});
+                const isValidEmail3 = await registrationSchema.findOne({member3Email: email, hackathon: true});
+                if(isValidEmail){
+                    return res.status(200).json({status: true});
+                }
+                if(isValidEmail2){
+                    return res.status(200).json({status: true});
+                }
+                if(isValidEmail3){
+                    return res.status(200).json({status: true});
+                }
+            if(contact!==""){
+                const isValidPhone = await registrationSchema.findOne({teamLeaderPhone: contact, hackathon: true})
+                if(isValidPhone){
+                    return res.status(200).json({status: true})
+                }
             }
-            if (isValidEmail2) {
-                return res.status(200).json({ status: true });
-            }
-            if (isValidEmail3) {
-                return res.status(200).json({ status: true });
-            }
-            else {
-                return res.status(400).json({ status: false });
-            }
+            return res.status(200).json({status: false})
         }
-    } catch (err) {
+    }catch(err){
         next(err)
     }
 }
-module.exports.search = async (req, res, next) => {
-    try {
-        const { email, contact } = req.body;
-        if (contact === "") {
-            let isValidEmail = await registrationSchema.findOne({ teamLeaderEmail: email, techexpert: true });
-            let isValidEmail2 = await registrationSchema.findOne({ member2Email: email, techexpert: true });
-            let isValidEmail3 = await registrationSchema.findOne({ member3Email: email, techexpert: true });
-            if (isValidEmail) {
-                return res.status(200).json({ status: true });
-            }
-            if (isValidEmail2) {
-                return res.status(200).json({ status: true });
-            }
-            if (isValidEmail3) {
-                return res.status(200).json({ status: true });
-            }
-            else {
-                isValidEmail = await registrationSchema.findOne({ teamLeaderEmail: email, nontechexpert: true });
-                isValidEmail2 = await registrationSchema.findOne({ member2Email: email, nontechexpert: true });
-                isValidEmail3 = await registrationSchema.findOne({ member3Email: email, nontechexpert: true });
-                if (isValidEmail) {
-                    return res.status(200).json({ status: true });
+module.exports.search = async(req,res,next)=>{
+    try{
+        const {email, contact} = req.body;
+            if(contact === ""){
+                let isValidEmail = await registrationSchema.findOne({teamLeaderEmail: email, techexpert: true});
+                let isValidEmail2 = await registrationSchema.findOne({member2Email: email, techexpert: true});
+                let isValidEmail3 = await registrationSchema.findOne({member3Email: email, techexpert: true});
+                if(isValidEmail){
+                    return res.status(200).json({status: true});
                 }
-                if (isValidEmail2) {
-                    return res.status(200).json({ status: true });
+                if(isValidEmail2){
+                    return res.status(200).json({status: true});
                 }
-                if (isValidEmail3) {
-                    return res.status(200).json({ status: true });
+                if(isValidEmail3){
+                    return res.status(200).json({status: true});
                 }
-                return res.status(400).json({ status: false });
+                else{
+                    isValidEmail = await registrationSchema.findOne({teamLeaderEmail: email, nontechexpert: true});
+                    isValidEmail2 = await registrationSchema.findOne({member2Email: email, nontechexpert: true});
+                    isValidEmail3 = await registrationSchema.findOne({member3Email: email, nontechexpert: true});
+                    if(isValidEmail){
+                        return res.status(200).json({status: true});
+                    }
+                    if(isValidEmail2){
+                        return res.status(200).json({status: true});
+                    }
+                    if(isValidEmail3){
+                        return res.status(200).json({status: true});
+                    }
+                    return res.status(400).json({status: false});
+                }
             }
-        }
-        if (email === "") {
-            let isValidPhone = await registrationSchema.findOne({ teamLeaderPhone: contact, techexpert: true });
-            if (isValidPhone) {
-                return res.status(200).json({ status: true })
-            } else {
-                isValidPhone = await registrationSchema.findOne({ teamLeaderPhone: contact, nontechexpert: true });
-                if (isValidPhone) {
-                    return res.status(200).json({ status: true })
-                } else {
-                    return res.status(200).json({ status: false })
+            if(email === ""){
+                let isValidPhone = await registrationSchema.findOne({teamLeaderPhone: contact, techexpert: true});
+                if(isValidPhone){
+                    return res.status(200).json({status: true})
+                }else{
+                    isValidPhone = await registrationSchema.findOne({teamLeaderPhone: contact, nontechexpert: true});
+                    if(isValidPhone){
+                        return res.status(200).json({status: true})
+                    }else{
+                        return res.status(200).json({status: false})
 
+                    }
                 }
             }
-        }
-    } catch (err) {
+        }catch(err){
         next(err)
     }
 }
