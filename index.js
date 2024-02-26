20e97474482fbd92ed6a144bdba243539ff6d934
Gauravved <gaurav.rpandit1@gmail.com>
2024-02-26 19:14:55 +0530

    Ready to deploy with live payment gateway

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const app = express();
 const http = require('http');
 const cors = require('cors');
 const { registration, order, verification, search } = require('./controller/registrationController');
-mongoose.connect('mongodb+srv://avinya2024:ChineseShadow24@cluster0.iaxmjpi.mongodb.net/avinya').then(() => {
+require('dotenv').config()
+mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log("Connected");
 }).catch((err) => {
     console.log(err);
 });
-require('dotenv').config()
 app.use(express.json());
 app.use(cors({
-    origin: "*"
+    origin: "https://avinya2024.live"
 }));
 app.options('*', cors());
 app.use("/*", (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // You can specify your allowed origin here
+    res.header('Access-Control-Allow-Origin', 'https://avinya2024.live'); // You can specify your allowed origin here
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials', 'true');
