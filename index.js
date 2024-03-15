f96d00f7361e23e2fe9bdcab51eaea2acc960657
Gauravved <gaurav.rpandit@mindsclik.com>
2024-03-15 14:15:35 +0530

    Removed QR

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 });
 app.use(express.json());
 app.use(cors({
-    origin: "*"
+    origin: "https://avinya2024.live"
 }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.options('*', cors());
 app.use("/*", (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://avinya2024.live');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials', 'true');
